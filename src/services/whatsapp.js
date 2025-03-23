79216147edf706ec89b326f7aa3116431df0d23d
mathazel <jeanmateuss@hotmail.com>
2025-03-23 17:30:37 -0300

    build:add webcrypto dependency

--- src/services/whatsapp.js
+++ src/services/whatsapp.js
@@ -1,3 +1,4 @@
+
 const { 
     makeWASocket, 
     useMultiFileAuthState, 
@@ -11,6 +12,8 @@ const {
     botName
 } = require('../config/config');
 const { handleMessageUpsert } = require('../core/message-handler');
+const { Crypto } = require('@peculiar/webcrypto');
+global.crypto = new Crypto();
 
 let currentSocket = null;
 let reconnectAttempts = 0;
